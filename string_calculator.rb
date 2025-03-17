efb03b9a83169949e9412f65ec64a3caa0bf5bee
tikhandesanket <sanket.tikhande@gmail.com>
2025-03-17 23:58:25 +0530

    added test cases for string calculation

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,1 +1,14 @@
 # In this file, we will add logic and a method that will return the execution result.
+class StringCalculator
+  def self.add(numbers)
+    return 0 if numbers.empty?
+    array_in_numbers = numbers.scan(/-?\d+|[a-zA-Z]+/).map{|ele| ele.to_i}
+    return 0 if array_in_numbers.empty?
+    negative_nums = array_in_numbers.select(&:negative?).empty?
+    if !negative_nums.empty?
+      raise "negative number not allowed"
+    else
+      return array_in_numbers.map{|num| num.to_i}.sum
+    end
+  end
+end
--- test_TDD_BDD.rb
+++ test_TDD_BDD.rb
@@ -1,1 +1,12 @@
 # In this file, we will add test cases to where three laws for test cases will be followed
+require 'minitest/autorun'
+require_relative 'string_calculator'
+class TestStringCalculator < Minitest::Test
+  def test_empty_string
+    assert_equal 0, StringCalculator.add("")
+  end
+
+  def test_single_number
+    assert_equal 1, StringCalculator.add("1")
+  end
+end
